116a667b759d9c2c60bdc5a27ce7e399090c43e6
Marko Milosevic <markom@niri-ic.com>
2026-05-27 17:25:57 +0200

    Add @NullMarked annotation to JwtAuthenticationFilter and update imports

--- src/main/java/rs/ac/ni/pmf/rwa/bookstore/security/JwtAuthenticationFilter.java
+++ src/main/java/rs/ac/ni/pmf/rwa/bookstore/security/JwtAuthenticationFilter.java
@@ -5,6 +5,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.jspecify.annotations.NullMarked;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -15,6 +16,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 
 @RequiredArgsConstructor
+@NullMarked
 public class JwtAuthenticationFilter extends OncePerRequestFilter
 {
 	private final JwtTokenUtil _jwtTokenUtil;
